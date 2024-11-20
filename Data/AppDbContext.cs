b18c890ec588efb91f79adb78c6f7b1b445971e0
Gustavo Rosa <gustavo.rosa@Ebix.com>
2024-11-20 09:37:02 -0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ApiLogin.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiLogin.Data
+{
+    // IdentityDbContext == Projetado especificamente para gerenciar informações relacionadas à identidade do usuário,
+    // como usuários, roles, claims e logins. Fornece uma série de funcionalidades prontas para uso.
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+
+    // DbContext == Permite criar um modelo de dados personalizado, adaptando-o às necessidades específicas da sua aplicação.
+    //public class AppDbContext : DbContext
+    //{
+    //    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    //    {
+    //    }
+
+    //    public DbSet<User> Users { get; set; }
+    //}
+}
---
+++ Model/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ApiLogin.Model
+{
+    public class User : IdentityUser
+    {
+        public string? CampoAdicional { get; set; }
+    }
+}
